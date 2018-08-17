96de149bceccaf4ad43c20bc256597f4bd606f51
Phil <phil@htglearn.com>
2018-08-17 16:51:19 -0400

    Added make place and extract place

--- fun_files.py
+++ fun_files.py
@@ -1,11 +1,36 @@
 # This is a program that lets me experiment with file stuff
 
 import os
+os.chdir('C:/Users/pwheeler')
 
-print(os.listdir('css'))
-os.mkdir('organized')
+# print(os.listdir('C:/Users/pwheeler/Pictures'))
+# os.mkdir('organized')
+# os.rename('C:/Users/pwheeler/ACHE.jpg','C:/Users/pwheeler/Pictures/ACHE.jpg')
+# print (os.getcwd())
 
 
+# def extract_place (a):
+#     loop=0
+#     position = []
+#     places = []
+#     for i in a:
+#         if a[loop]=="_":
+#             position.append(loop)
+#             loop=loop+1
+#         else:
+#             loop=loop+1
+#     # print (position[0])
+#     places.append(a[position[0]+1:position[1]])
+#     # print (places)
+#     print (loop)
+#     print (position)
+#     print (places)
 
+def extract_place (a):
+    return a.split("_")[0:]
 
+def make_place_dir (a):
+    for i in a:
+        os.mkdir(i)
 
+print (extract_place("2018-06-06_MountainView_16:20:00.jpg"))
